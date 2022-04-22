665eca4f64850499d009fbcd08898877dd72ce7f
shafayatC <shafayatplay@gmail.com>
2022-04-22 15:29:43 +0600

    update github with licenc & slider.giv & readme

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+//import SimpleCarouselSlider from 'react-simple-carousel-image-slider';
+
 import SimpleCarouselSlider from './components/rscis/SimpleCarouselSlider';
 
 function App() {
@@ -5,14 +8,18 @@ function App() {
   var images = [
     require('./img/1.jpg'),
     require('./img/2.jpg'),
+    require('./img/3.jpg'),
     require('./img/4.jpg'),
-    require('./img/6.jpg')
-];
+    require('./img/5.jpg')
+  ];
   return (
     <div className="App">
-
-      <SimpleCarouselSlider images={images} autoplay={false}/>
-
+      <div style={{ width: "1100px", margin: "auto" }}>
+        <SimpleCarouselSlider
+          images={images}
+          autoplay={false}
+        />
+      </div>
     </div>
   );
 }
