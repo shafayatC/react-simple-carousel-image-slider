407a8a117026f2ee80a7099dc0bc9976429c4d52
shafayatC <shafayatplay@gmail.com>
2022-05-18 12:10:52 +0600

    push for gh publish

---
+++ example/build/precache-manifest.6e84a6f308dc20571641d029939a6358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa25142fbdd0af030dabf3efbaf92f5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "765e81f18a4507041d82",
+    "url": "./static/css/main.40deb9fc.chunk.css"
+  },
+  {
+    "revision": "d1eb51d9e339abb2914a",
+    "url": "./static/js/2.c4eb6eea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c4eb6eea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765e81f18a4507041d82",
+    "url": "./static/js/main.a207fa7f.chunk.js"
+  },
+  {
+    "revision": "a32db419cc79f6497796",
+    "url": "./static/js/runtime-main.507ea80c.js"
+  }
+]);
